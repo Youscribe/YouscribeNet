eab3c058a8cbe3b01492f18710b3309604e9e61d
Yann <me.show@gmail.com>
2015-04-15 10:20:04 +0200

    make hmac public

--- YouScribe.Rest/YousScribeHMACHttpClientDecorator.cs
+++ YouScribe.Rest/YousScribeHMACHttpClientDecorator.cs
@@ -10,7 +10,7 @@ using System.Threading;
 
 namespace YouScribe.Rest
 {
-    class YousScribeHMACHttpClientDecorator : YousScribeHttpClient
+    public class YousScribeHMACHttpClientDecorator : YousScribeHttpClient
     {
         byte[] secretKey;
         int applicationId;
